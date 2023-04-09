9229b0ff13be36289e670cfa7ed3a7f8915f8b75
Atreyye12 <102455332+Atreyye12@users.noreply.github.com>
2023-04-09 09:23:51 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 video = "";
-
+status = "";
 function setup() {
     video = createCapture(VIDEO);
     video.size(550, 500);
@@ -7,10 +7,32 @@ function setup() {
     canvas = createCanvas(550, 550);
     canvas.position(560,150);
   
+    
   }
 function draw(){
     image(video, 0, 0, 480, 380);
+    if(status !=""){
+        objectDetector.detect(gotResults);
+        for (i = 0; i < objects.length; i++){
+            document.getElementById("status").innerHTML = "Status : Object Detected";
+            document.getElementById("number_of_objects").innerHTML = "Number of Objects Detected" + objects.length;
+
+            fill("#FF0000");
+            percent = floor(objects[i].confidence * 100);
+            text(objects[i].label + "" + percent + "%", objects[i].x + 15, objects[i].y + 15);
+            nofill();
+            stroke("#FF0000");
+            rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
+        }
+    }
 }
+function gotResults(error, results){
+    if(error){
+        console.log(error);
+    }
+    console.log(results);
+    object = results;
+    }
 function start(){
     objectDetector = ml5.objectDetector('cocossd', modelLoaded);
     document.getElementById("status").innerHTML = "Status : Detecting objects";
